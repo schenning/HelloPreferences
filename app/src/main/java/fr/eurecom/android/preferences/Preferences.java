11e0dff53306a1316eae01bcf4a8d3290001ba4b
schenning <henning.schei@gmail.com>
2015-11-30 12:03:48 +0100

    ewf

--- app/src/main/java/fr/eurecom/android/preferences/Preferences.java
+++ app/src/main/java/fr/eurecom/android/preferences/Preferences.java
@@ -3,5 +3,25 @@ package fr.eurecom.android.preferences;
 /**
  * Created by schei on 11/29/15.
  */
-public class Preferences {
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+
+
+public class Preferences extends PreferenceActivity {
+    /**
+     * Called when the activity is first created.
+     */
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //load the preferences from an xml rsource
+        addPreferencesFromResource(R.xml.preferences);
+    }
 }
+
+
+
+
+
+
+
