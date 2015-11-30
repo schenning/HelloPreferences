e76a0567d99d08aecbe4d3e30b2ec275e5e89078
schenning <henning.schei@gmail.com>
2015-11-30 19:17:29 +0100

    netflic and chill soon

--- app/src/main/java/fr/eurecom/android/preferences/HelloPreferences.java
+++ app/src/main/java/fr/eurecom/android/preferences/HelloPreferences.java
@@ -1,37 +1,86 @@
 package fr.eurecom.android.preferences;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class HelloPreferences extends AppCompatActivity {
 
-    @Override
+    //* @Override *//
+    SharedPreferences preferences;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_preferences);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
-
-
-
-
-        /**FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // Initialize preferences
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        Button button1 = (Button) findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                String username = preferences.getString("username", "n/a");
+                String password = preferences.getString("password", "n/a");
+                // A toast is a view containing a quick little message for the user.
+                showPrefs(username, password);
             }
-        });**/
+        });
+        Button button2 = (Button)findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+        /**Button button4 = (Button)findViewById(R.id.buttonCN); **/
     }
+
+    private void showPrefs(String username, String password) {
+        Toast.makeText(HelloPreferences.this,
+                "You kept user: " + username + " and password: " + password,
+                Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // We have only one menu option
+            case R.id.action_settings:
+                // Launch Preference activity
+                Intent i = new Intent(HelloPreferences.this, Preferences.class);
+                startActivity(i);
+                // A toast is a view containing a quick little message for the user.
+                Toast.makeText(HelloPreferences.this,
+                        "Here you can store your user credentials.",
+                        Toast.LENGTH_LONG).show();
+                Log.i("Main", "sent an intent to the Preference class");
+                break;
+        }
+        return true;
+    }
+
+
+    /**
+     * FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+     * fab.setOnClickListener(new View.OnClickListener() {
+     *
+     * @Override public void onClick(View view) {
+     * Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+     * .setAction("Action", null).show();
+     * }
+     * });
+     **/
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -40,18 +89,39 @@ public class HelloPreferences extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    /**
+     * @Override public boolean onOptionsItemSelected(MenuItem item) {
+     * // Handle action bar item clicks here. The action bar will
+     * // automatically handle clicks on the Home/Up button, so long
+     * // as you specify a parent activity in AndroidManifest.xml.
+     * int id = item.getItemId();
+     * <p>
+     * //noinspection SimplifiableIfStatement
+     * if (id == R.id.action_settings) {
+     * return true;
+     * }
+     * <p>
+     * return super.onOptionsItemSelected(item);
+     * }
+     **/
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    public void openDialog(View v) {
+        // Create out AlterDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Reset username and password?");
+        builder.setCancelable(true);
+        builder.setPositiveButton("I agree", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Resetting Credential ", Toast.LENGTH_LONG).show();
+            }
+        });
+        builder.setNegativeButton("No, no", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Keep Credential", Toast.LENGTH_LONG).show();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
 
-        return super.onOptionsItemSelected(item);
     }
 }
